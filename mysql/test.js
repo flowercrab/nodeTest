49e20dab49744391fcbe20f21081ed000c83dd99
flowercrab <kaewoon77@gmail.com>
2020-07-24 16:38:37 +0900

    1stcommit

---
+++ mysql/test.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+   host : '13.124.197.94',
+   user : 'loivie_user',
+   password : 'company2@',
+   database : 'loivie_db'
+});
+
+conn.connect();
+
+
+
